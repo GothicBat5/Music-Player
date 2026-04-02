c015004160b832ea054ba13dcb72a7ff841e8d02
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-04-02 19:08:55 +0800

    Update player.rb

--- Red Stone/player.rb
+++ Red Stone/player.rb
@@ -1,9 +1,35 @@
+
 class Player
+  attr_reader :playing, :current_song, :volume
+
+  def initialize
+    @playing = false
+    @current_song = nil
+    @volume = 80   # 0-100
+  end
+
   def play(song)
-    # Audio logic will go here later
+    @current_song = song
+    @playing = true
+    # TODO: start audio stream
+    log "Playing: #{song}"
   end
 
   def stop
-    # Stop logic later
+    @playing = false
+    # TODO: stop audio stream
+    log "Stopped"
+  end
+
+  def set_volume(level)
+    @volume = level.clamp(0, 100)
+    # TODO: apply to audio backend
+    log "Volume: #{@volume}%"
+  end
+
+  private
+
+  def log(msg)
+    puts "[Player] #{msg}"
   end
 end
