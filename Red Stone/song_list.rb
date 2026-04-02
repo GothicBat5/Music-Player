e48f73fa43f77de4757db5aca43717aee57b2dee
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-04-02 19:09:17 +0800

    Update song_list.rb

--- Red Stone/song_list.rb
+++ Red Stone/song_list.rb
@@ -1,22 +1,40 @@
+# frozen_string_literal: true
+
 require 'fox16'
 require_relative 'theme'
 include Fox
 
 class SongList < FXList
+  TRACKS = [
+    { title: "Sanctuary", artist: "Joji"},
+    { title: "ASMR Ear Whisper", artist: "Unknown" },
+    { title: "K", artist: "Cigarette After Sex"},
+    { title: "FANCY", artist: "TWICE"},
+    { title: "Tadow", artist: "Masego"},
+    { title: "Glimpse of Us", artist: "Joji"},
+    { title: "SLOW DANCING IN THE DARK", artist: "Joji"},
+    { title: "Die For You", artist: "The Weeknd"},
+  ].freeze
+
   def initialize(parent, app)
-    super(parent, opts: LIST_SINGLESELECT | LAYOUT_FILL_Y | LAYOUT_FILL_X)
+    super(parent,
+          opts: LIST_SINGLESELECT | LAYOUT_FILL_Y | LAYOUT_FILL_X | FRAME_NONE)
 
     self.backColor = Theme::LIST_BG
     self.textColor = Theme::LIST_TEXT
     self.font = Theme.list_font(app)
-    self.numVisible = 6
+    self.numVisible = TRACKS.size
 
-    appendItem("Sanctuary - Joji")
-    appendItem("ASMR Ear Whisper")
-    appendItem("K - Cigarette After Sex")
-    appendItem("FANCY - TWICE")
-    appendItem("Tadow - Masego")
+    TRACKS.each_with_index do |track, i|
+      num = format("%02d", i + 1)
+      appendItem("  #{num}  #{track[:title]}  –  #{track[:artist]}")
+    end
 
-    self.currentItem = 1   # Static selected item
+    self.currentItem = 0
+  end
+
+  def selected_track
+    idx = currentItem
+    idx >= 0 ? TRACKS[idx] : nil
   end
 end
