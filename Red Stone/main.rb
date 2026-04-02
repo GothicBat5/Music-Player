c40128fa6166f1333f5fe365ee2e2befcaeddc3b
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-04-02 19:06:22 +0800

    Update main.rb

--- Red Stone/main.rb
+++ Red Stone/main.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'fox16'
 include Fox
 
 require_relative 'main_window'
 
-app = FXApp.new
+app = FXApp.new("Wavelength", "MusicPlayer")
 window = MainWindow.new(app)
 
 app.create
