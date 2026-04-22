9c9f18ae62a1c80ebd891e90114471e004de9c25
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-04-22 16:17:01 +0800

    Create  binary.c

---
+++ Miscellaneous/low_level /binary.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <time.h>
+
+int read_num()
+{
+    char input[100];
+    char *end;
+    long value;
+    
+    while(1)
+    {
+        printf("<Lines> : ");
+        
+        if(!fgets(input, sizeof(input), stdin))
+        {
+            continue;
+        }
+        
+        value = strtol(input, &end, 10);
+        
+        if(end == input || *end != '\n')
+        {
+            printf("Invalid Input.");
+        }
+        
+        else if(value < 0)
+        {
+            printf("Not valid input");
+        }
+        else
+        {
+            return (int) value;
+        }
+    }
+}
+
+int main()
+{
+    int n = read_num();
+    
+    srand(time(NULL));
+    
+    clock_t start = clock();
+    
+    char buffer[10];
+    
+    for(int i = 1; i < n; i++)
+    {
+        
+        for(int j = 0; j < 9; j++)
+        {
+            
+            buffer[j] = (rand() % 2 )? '1' : '0';
+        }
+        
+        buffer[9] = '\0';
+        
+        printf("[ %d. ] = %s\n", i, buffer);
+    }
+    
+    double elapsed = (double) (clock() - start) / CLOCKS_PER_SEC;
+    printf("\n\nTime elapsed: %f seconds\n", elapsed);
+    
+    return 0; 
+}
+
