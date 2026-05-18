3c99b53fa57f8abc8ea264378122d7613b3db7b6
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-18 22:08:10 +0800

    Update sys.c

--- Vol /sys.c
+++ Vol /sys.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <alsa/asoundlib.h>
-#include "volume.v"
+#include "volume.h"
 
 int main(int argc, char *argv[]) 
 {
@@ -24,21 +24,16 @@ int main(int argc, char *argv[])
     snd_mixer_attach(handle, "default");
     snd_mixer_selem_register(handle, NULL, NULL);
     snd_mixer_load(handle);
-
     //identify the master control
     snd_mixer_selem_id_malloc(&sid);
     snd_mixer_selem_id_set_index(sid, 0);
     snd_mixer_selem_id_set_name(sid, "Master");
     elem = snd_mixer_find_selem(handle, sid);
-
     snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-
     //ercentage to hardware range
     volume = (target * (max - min) / 100) + min;
-
     //s volume
     snd_mixer_selem_set_playback_volume_all(elem, volume);
-
     //cleanup
     snd_mixer_close(handle);
     snd_mixer_selem_id_free(sid);
