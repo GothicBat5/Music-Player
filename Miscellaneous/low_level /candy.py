96f451a3afed5c706206419881df66162b3afd91
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-05-26 00:45:35 +0800

    Create candy.py

---
+++ Miscellaneous/low_level /candy.py
@@ -0,0 +1,23 @@
+import os 
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir()
+    if file == "candy.py" or file == "thekey.key":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as key:
+    secretkey = key.read()
+    
+for file in files:
+    with open(file, "rb") open thefile:
+        contents = thefile.read()
+    contents.decrypted = Fernet(secreykey).decrypt.(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
+        
