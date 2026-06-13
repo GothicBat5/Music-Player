2dfa1053cf061e36b8bcb17475617732924af64d
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-13 19:49:32 +0800

    Create increase_per.rb

---
+++ increase_per.rb
@@ -0,0 +1,15 @@
+# Origianl = 100, New = 150, = 50% increased! 
+
+print "Original value: "
+original = gets.chomp.to_f
+
+print "New value: "
+new_value = gets.chomp.to_f
+
+increase = new_value - original
+
+percentage = (increase / original) * 100
+
+puts "\nIncrease: #{increase}"
+
+puts "\nPercentage increase: #{percentage.round(2)}%"
