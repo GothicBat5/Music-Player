37c218a21f6e82cf83ee4e0caff1d6f3f8b4a234
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-13 19:40:18 +0800

    Create get_per_val.rb

---
+++ get_per_val.rb
@@ -0,0 +1,11 @@
+#What is A% of B? 
+
+print "Percent: "
+percent = gets.to_f
+
+print "Number: "
+number = gets.to_f
+
+result = (percent / 100) * number
+
+puts "\n#{percent}% of #{number} = #{result}"
