e9d969849c279865cd6ecf1427161f2d651cf899
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-07-05 22:11:57 +0800

    Update at_species.py

--- Miscellaneous/at_species.py
+++ Miscellaneous/at_species.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np 
-
 import matplotlib.pyplot as plt  
 
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
@@ -8,13 +7,11 @@ from tensorflow.keras.preprocessing import image
 from PIL import Image 
 
 model = MobileNetV2(weights= 'imagenet')
-
-img_path = 'sms.jpg' #image source
+img_path = 'sms.jpg' #image source from the file ::
 img = image.load_img(img_path, target_size = (224, 224))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array = preprocess_input(img_array)
-
 preds = model.predict(img_array)
 decoded = decode_predictions(preds, top=3)[0]
 
@@ -26,5 +23,3 @@ plt.show()
 print("\nTop 3 Predictions")
 for i, (imagenetID, label, prob) in enumerate(decoded): 
     print(f"{i+1}.{label} ({round(prob*100, 2)}%)")
-
-
