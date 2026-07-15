8ee6c2d86f73fea0d28eb5323517917acc1028ac
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-07-15 15:08:34 +0800

    Create update.rb

---
+++ Gems/update.rb
@@ -0,0 +1,34 @@
+
+require "rubygems"
+
+if ARGV.include?("-h") || ARGV.include?("--help")
+  warn "rubygems_update [options]"
+  warn ""
+  warn "This will install the latest version of RubyGems."
+  warn ""
+  warn "\t--version=X.Y\tUpdate RubyGems from the X.Y version."
+  exit
+  
+end
+
+unless ARGV.grep(/--version=([\d\.]*)/).empty?
+  exec Gem.ruby, "-S", $PROGRAM_NAME, "_#{$1}_"
+  
+end
+
+update_dir = $LOAD_PATH.find {|dir| dir.include?("rubygems-update") }
+
+if update_dir.nil?
+  puts "Error: Cannot find RubyGems update path!"
+  puts
+  puts "RubyGems has already been updated."
+  puts "The rubygems-update gem may now be uninstalled."
+  puts "E.g.    gem uninstall rubygems-update"
+  
+else
+  update_dir = File.dirname(update_dir)
+  Dir.chdir update_dir
+  ENV["GEM_PREV_VER"] = Gem::VERSION
+  abort unless system(Gem.ruby, "--disable-gems", "setup.rb", *ARGV)
+  
+end
