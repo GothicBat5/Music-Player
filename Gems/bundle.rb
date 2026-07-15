83d119f1bc141ddf6297a9e961fecfa7a91c2c6a
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-07-15 15:09:21 +0800

    Create bundle.rb

---
+++ Gems/bundle.rb
@@ -0,0 +1,28 @@
+
+Signal.trap("INT") do
+  Bundler.ui.debug("\n#{caller.join("\n")}") if defined?(Bundler)
+  exit 1
+  
+end
+
+base_path = File.expand_path("../lib", __dir__)
+
+if File.exist?(base_path)
+  $LOAD_PATH.unshift(base_path)
+  
+end
+
+require "bundler"
+require "bundler/friendly_errors"
+
+Bundler.with_friendly_errors do
+  require "bundler/cli"
+
+
+  help_flags = %w[--help -h]
+  help_flag_used = ARGV.any? {|a| help_flags.include? a }
+  args = help_flag_used ? Bundler::CLI.reformatted_help_args(ARGV) : ARGV
+
+  Bundler::CLI.start(args, debug: true)
+  
+end
